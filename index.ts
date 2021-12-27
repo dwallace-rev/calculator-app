6d8af3772d83f61a4153c871efeba589633c15c0
dwallace-rev <david.wallace@revature.net>
2021-12-27 11:41:15 -0500

    added sub funcitonality to index

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import { add, sub } from './calc-functions';
 
 const app = express();
 
 app.get('add/:num1/:num2', (req, res)=>{
     const {num1, num2} = req.params;
     const sum = add(Number(num1), Number(num2));
+    res.send(sum);
 });
+
+app.get('sub/:num1/:num2', (req, res)=>{
+    const {num1, num2} = req.params;
+    const dif = sub(Number(num1), Number(num2));
+    res.send(dif);
+})
 
 app.listen(3000, () => "Application started");
